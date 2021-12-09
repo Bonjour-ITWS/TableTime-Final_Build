dd1d96701ef816f3fc86e046d3694cdee6e1fe87
Tyler Haynes <72038702+haynet3@users.noreply.github.com>
2021-12-09 11:55:24 -0500

    Add files via upload

---
+++ TableTime-Database/tabletime.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2021 at 05:55 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tabletime`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `has_reservation` tinyint(1) NOT NULL,
+  `Floor` varchar(255) NOT NULL,
+  `Seat` int(255) NOT NULL,
+  `Start` time NOT NULL,
+  `End` time NOT NULL,
+  `has_extended` tinyint(1) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`username`, `password`, `has_reservation`, `Floor`, `Seat`, `Start`, `End`, `has_extended`, `is_admin`) VALUES
+('dbachi3', 'Test', 0, '', 0, '00:00:00', '00:00:00', 0, 0),
+('haynet3', 'AMC', 0, '', 0, '00:00:00', '00:00:00', 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD UNIQUE KEY `username` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
